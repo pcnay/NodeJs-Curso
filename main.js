cabcd5e28df046a62b7626f44e9ecb4ae315ebcf
DevOps-PcNay <121329265+DevOps-PcNay@users.noreply.github.com>
2025-05-20 02:24:23 +0000

    Se termina la seccion del tema NPM

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
 // Llamando el modulo, usando el tipo de llamado CommonJS
 const Operadores = require("./Src/Math.js")
+const Es_numero = require("isnumber")
+
 
 
 // Llamando a las funciones:
 console.log(`La suma = ${Operadores.add(5,9)}; la resta = ${Operadores.subtract(50,100)}`)
+console.log (`Es numero : -Cadena- ${Es_numero("Cadena")}`)
+
 
 
 
--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import { Multiply, Divide } from "./Src/Maths-ECMod.mjs"
 
+
 // Llamadoo de la funcion EC Module.
 console.log (`La multiplicacion es = ${Multiply(4,3)}; la division es = ${Divide(100,2)}`)
 
